cd0ef5084eb477b2059572cd66a59490f131dfcf
geraud <gbeguin@xebia.fr>
2020-03-04 01:33:16 +0100

    add Up navigation

--- app/src/main/java/fr/geraud/mycplusplusmovies/MainActivity.kt
+++ app/src/main/java/fr/geraud/mycplusplusmovies/MainActivity.kt
@@ -2,13 +2,19 @@ package fr.geraud.mycplusplusmovies
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val navController by lazy { findNavController(R.id.mainHostFragment) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp() || super.onSupportNavigateUp()
 
 }
